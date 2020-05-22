44f17b7e35dbb169547077b19c1eb293923d76ea
Carol42 <63017741+Carol42@users.noreply.github.com>
2020-05-22 18:11:41 -0300

    Segunda versão do site
    Mudanças no design e efeitos

---
+++ funcoes.js
@@ -0,0 +1,14 @@
+function typeWrite(elemento){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = ' ';
+    textoArray.forEach(function(letra, i){   
+      
+    setTimeout(function(){
+        elemento.innerHTML += letra;
+    }, 75 * i)
+
+  });
+}
+const eu = document.querySelector('.precioso');
+typeWrite(eu);
+
